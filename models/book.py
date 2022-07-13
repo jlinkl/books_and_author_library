1eef6e437b6166371e4082c656329d4638bda58a
James <james.m.linklater@gmail.com>
2022-07-13 14:34:16 +0100

    first commit

---
+++ models/book.py
@@ -0,0 +1,9 @@
+from re import T
+
+
+class Book:
+    def __init__(self, title, genre, author, id=None):
+        self.title = title
+        self.genre = genre
+        self.author = author
+        self.id = id
